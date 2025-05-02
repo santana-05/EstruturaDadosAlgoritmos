41450f6d793ae767a336c1312970d4d6b9ca1497
Santana <mauriciosantanadasilva123@gmail.com>
2025-05-02 00:09:10 +0000

    Leitura do arquivo

--- LendoArquivo.c
+++ LendoArquivo.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#define TAM 600
 
 int main(){
-   printf("Hello world \n"); 
+   FILE *arquivo;
+   int num1, num2;
    
+   arquivo = fopen("L0Q1.in", "r"); // abre o arquivo
+   
+
+
+   if (arquivo == NULL) {
+      printf("Erro ao abrir arquivo.\n");
+      return 1;
+   }else {
+         printf("Arquivo aberto com sucesso.\n");
+   }
+
+   while (fscanf(arquivo, "%d %d", &num1, &num2) == 2) {
+      printf("(%d, %d)\n", num1, num2);
+   }
+
+   fclose(arquivo);
+   return 0;
 }
