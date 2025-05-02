c1bd8ea33a4b728370c2d92ef13b29b965a4dfba
Santana <mauriciosantanadasilva123@gmail.com>
2025-05-02 02:26:50 +0000

    Lendo e imprimindo todos os números

--- LendoArquivo.c
+++ LendoArquivo.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#define TAM 600
+#include <stdlib.h>
+#include <ctype.h>
 
 int main(){
    FILE *arquivo;
-   int num1, num2;
+   int nums[1000]; //armazena todos os números lidos
+   int count = 0;
+   char c;
    
    arquivo = fopen("L0Q1.in", "r"); // abre o arquivo
-   
-
 
    if (arquivo == NULL) {
       printf("Erro ao abrir arquivo.\n");
@@ -16,9 +17,22 @@ int main(){
          printf("Arquivo aberto com sucesso.\n");
    }
 
-   while (fscanf(arquivo, "%d %d", &num1, &num2) == 2) {
-      printf("(%d, %d)\n", num1, num2);
-   }
+   // Lê todos os números do arquivo
+   while ((c = fgetc(arquivo)) != EOF) {
+      if (isdigit(c) || c == '-') {
+          ungetc(c, arquivo);
+          if (fscanf(arquivo, "%d", &nums[count]) == 1) {
+              count++;
+          }
+      }
+  }
+
+  // Imprime os pares (x, y)
+  for (int i = 0; i < count; i += 2) {
+      if (i + 1 < count) { // Verifica se há um par completo
+          printf("(%d, %d)\n", nums[i], nums[i + 1]);
+      }
+  }
 
    fclose(arquivo);
    return 0;
