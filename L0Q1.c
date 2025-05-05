f2c2a800fbad6ca368e889ee117d363973935846
Santana <mauriciosantanadasilva123@gmail.com>
2025-05-05 02:05:52 +0000

    ALTERANDO

--- L0Q1.c
+++ L0Q1.c
@@ -1,114 +1,115 @@
-// Maurício Santana e Daniel Santana
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <string.h>
+#include <math.h>
 
-#define TAM_STR 500
-#define TAM_COORD 300
+#define MAX_LINHA 500
+#define MAX_PONTOS 300
 
 typedef struct {
-    int x, y;
-} Ponto;
+    int x;
+    int y;
+} Coordenada;
 
-float dist_origem(Ponto p) {
-    return sqrt(p.x * p.x + p.y * p.y);
+float calcularDistanciaOrigem(Coordenada pt) {
+    return sqrt(pt.x * pt.x + pt.y * pt.y);
 }
 
-float dist_entre(Ponto a, Ponto b) {
-    int dx = b.x - a.x;
-    int dy = b.y - a.y;
-    return sqrt(dx * dx + dy * dy);
+float calcularDistancia(Coordenada a, Coordenada b) {
+    int deltaX = a.x - b.x;
+    int deltaY = a.y - b.y;
+    return sqrt(deltaX * deltaX + deltaY * deltaY);
 }
 
-int extrair_pontos(const char *linha, Ponto pontos[]) {
-    int x, y, n = 0, offset;
-    const char *ptr = linha;
+int parsearPontos(const char *texto, Coordenada vetor[]) {
+    int qtd = 0;
+    int x, y, deslocamento;
+    const char *atual = texto;
 
-    while (*ptr) {
-        if (*ptr == '(') {
-            if (sscanf(ptr, " ( %d , %d ) %n", &x, &y, &offset) == 2 ||
-                sscanf(ptr, "(%d,%d)%n", &x, &y, &offset) == 2 ||
-                sscanf(ptr, " (%d ,%d) %n", &x, &y, &offset) == 2 ||
-                sscanf(ptr, " (%d,%d) %n", &x, &y, &offset) == 2) {
-                pontos[n].x = x;
-                pontos[n].y = y;
-                n++;
-                ptr += offset;
+    while (*atual) {
+        if (*atual == '(') {
+            if (sscanf(atual, "(%d,%d)%n", &x, &y, &deslocamento) == 2 ||
+                sscanf(atual, " (%d,%d)%n", &x, &y, &deslocamento) == 2 ||
+                sscanf(atual, "( %d , %d )%n", &x, &y, &deslocamento) == 2 ||
+                sscanf(atual, " ( %d , %d ) %n", &x, &y, &deslocamento) == 2) {
+                vetor[qtd].x = x;
+                vetor[qtd].y = y;
+                qtd++;
+                atual += deslocamento;
                 continue;
             }
         }
-        ptr++;
+        atual++;
     }
-    return n;
+
+    return qtd;
 }
 
-int comparar_dist_origem(const void *a, const void *b) {
-    Ponto *p1 = (Ponto *)a;
-    Ponto *p2 = (Ponto *)b;
-    float d1 = dist_origem(*p1);
-    float d2 = dist_origem(*p2);
-    if (d1 < d2) return -1;
-    if (d1 > d2) return 1;
-    return 0;
+int compararPorOrigem(const void *p1, const void *p2) {
+    Coordenada *a = (Coordenada *)p1;
+    Coordenada *b = (Coordenada *)p2;
+    float da = calcularDistanciaOrigem(*a);
+    float db = calcularDistanciaOrigem(*b);
+    return (da > db) - (da < db);
 }
 
-void imprimir_saida(FILE *saida, const Ponto pontos_ord[], int n, float d_total, float d_caminho) {
-    fprintf(saida, "points");
-    for (int i = 0; i < n; i++) {
-        fprintf(saida, " (%d,%d)", pontos_ord[i].x, pontos_ord[i].y);
+void escreverResultado(FILE *arq, Coordenada ordenados[], int total, float percurso, float atalho) {
+    fprintf(arq, "points");
+    for (int i = 0; i < total; i++) {
+        fprintf(arq, " (%d,%d)", ordenados[i].x, ordenados[i].y);
     }
-    fprintf(saida, " distance %.2f shortcut %.2f\n", d_total, d_caminho);
+    fprintf(arq, " distance %.2f shortcut %.2f\n", percurso, atalho);
 }
 
 int main() {
-    FILE *fp_in = fopen("L0Q1.in", "r");
-    FILE *fp_out = fopen("L0Q1.out", "w");
-    if (!fp_in || !fp_out) {
-        perror("Erro ao abrir arquivos");
-        return EXIT_FAILURE;
+    FILE *entrada = fopen("L0Q1.in", "r");
+    FILE *saida = fopen("L0Q1.out", "w");
+
+    if (!entrada || !saida) {
+        perror("Falha ao abrir arquivos");
+        return 1;
     }
 
-    char linha[TAM_STR];
-    Ponto pontos[TAM_COORD];
-    Ponto pontos_ord[TAM_COORD];
+    char buffer[MAX_LINHA];
+    Coordenada lista[MAX_PONTOS];
+    Coordenada ordenados[MAX_PONTOS];
 
-    while (fgets(linha, sizeof(linha), fp_in)) {
-        size_t len = strlen(linha);
-        if (len > 0 && linha[len - 1] == '\n') {
-            linha[len - 1] = '\0';
+    while (fgets(buffer, sizeof(buffer), entrada)) {
+        size_t tam = strlen(buffer);
+        if (tam > 0 && buffer[tam - 1] == '\n') {
+            buffer[tam - 1] = '\0';
         }
-    
-        int somente_espacos = 1;
-        for (size_t i = 0; i < len; i++) {
-            if (linha[i] != ' ' && linha[i] != '\t' && linha[i] != '\r') {
-                somente_espacos = 0;
+
+        int vazia = 1;
+        for (size_t i = 0; i < tam; i++) {
+            if (buffer[i] != ' ' && buffer[i] != '\t' && buffer[i] != '\r') {
+                vazia = 0;
                 break;
             }
         }
-        if (somente_espacos || linha[0] == '\0') continue;
+        if (vazia || buffer[0] == '\0') continue;
 
-        int n = extrair_pontos(linha, pontos);
-        if (n == 0) {
-            fprintf(fp_out, "points distance 0.00 shortcut 0.00\n");
+        int totalPontos = parsearPontos(buffer, lista);
+        if (totalPontos == 0) {
+            fprintf(saida, "points distance 0.00 shortcut 0.00\n");
             continue;
         }
 
-        for (int i = 0; i < n; i++) pontos_ord[i] = pontos[i];
-        qsort(pontos_ord, n, sizeof(Ponto), comparar_dist_origem);
+        memcpy(ordenados, lista, totalPontos * sizeof(Coordenada));
+        qsort(ordenados, totalPontos, sizeof(Coordenada), compararPorOrigem);
 
-        float distancia_total = 0.0;
-        for (int i = 0; i < n - 1; i++) {
-            distancia_total += dist_entre(pontos[i], pontos[i + 1]);
+        float distanciaPercorrida = 0;
+        for (int i = 0; i < totalPontos - 1; i++) {
+            distanciaPercorrida += calcularDistancia(lista[i], lista[i + 1]);
         }
 
-        float atalho = dist_entre(pontos[0], pontos[n - 1]);
+        float atalho = calcularDistancia(lista[0], lista[totalPontos - 1]);
 
-        imprimir_saida(fp_out, pontos_ord, n, distancia_total, atalho);
+        escreverResultado(saida, ordenados, totalPontos, distanciaPercorrida, atalho);
     }
 
-    fclose(fp_in);
-    fclose(fp_out);
+    fclose(entrada);
+    fclose(saida);
+
     return 0;
 }
